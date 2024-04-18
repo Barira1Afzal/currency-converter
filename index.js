a75b76256631e4e707d09205a140f5efa7200d40
Barira1Afzal <162354710+Barira1Afzal@users.noreply.github.com>
2024-04-18 23:56:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /user/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+const currency = {
+    USD: 1, // Base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let user_answer = await inquirer_1.default.prompt([
+    {
+        name: 'from',
+        message: "Enter From Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'to',
+        message: "Enter to Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'amount',
+        message: 'Enter your Amount',
+        type: 'number '
+    }
+]);
+console.log(user_answer);
+let fromAmount = currency[user_answer.from]; // exchange rate
+let toAmount = currency[user_answer.to]; // exchange rate
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD base currency //4
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
+console.log(fromAmount);
+console.log(toAmount);
+console.log(amount);
